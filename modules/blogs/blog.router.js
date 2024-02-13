4c77248ce1c529c44e0018c736b8858ae425c4f7
Saurav Giri <saurav130giri@gmail.com>
2024-02-13 13:58:18 +0545

    added blogs and categories

--- modules/blogs/blog.router.js
+++ modules/blogs/blog.router.js
@@ -4,6 +4,33 @@ router.get("/", (req, res) => {
   res.json({ msg: "hello from blog router" });
 });
 
+router.post("/", (req, res)=>{
+  console.log(req.body);
+    res.json({msg: "hello from post blog router"})
+});
+
+router.put("/:id",(req, res)=>{
+      const {id}=req.params;
+      const data=req.body;
+      console.log({id, data});
+      res.json({msg: "hello from put blog router"})
+});
+
+
+router.patch("/:id",(req,res)=>{
+  const {id}=req.params;
+  const data=req.body;
+  console.log({id, data});
+  res.json({msg:"hello from put blog router"});
+})
+
+router.delete("/:id",(req,res)=>{
+  const {id}=req.body;
+
+  console.log(req.params.id);
+  res.json({msg:"hello from blog router"});
+})
+
 
 
 module.exports= router;
